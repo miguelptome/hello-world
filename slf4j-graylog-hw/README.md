43d10a5d21cdf0d71e104b2c773f7d7d34ad24b3
Miguel Tomé <miguelptome@gmail.com>
2020-02-26 12:10:42 +0000

    First commit to SLF4J Graylog Hello world project.

---
+++ slf4j-graylog-hw/README.md
@@ -0,0 +1,15 @@
+# slf4j-graylog-hw
+
+Hello World with slf4j, sending logs to graylog demonstration.
+
+## Tools:
+* Maven 
+* java 1.8
+* SLF4J
+* logback
+
+## Description
+Uses simple demo, sending logs to graylog.
+
+## RUN
+Run as Java Application
---
+++ slf4j-graylog-hw/src/main/java/pt/courses/slf4j/LoggerRoot.java
@@ -0,0 +1,24 @@
+package pt.courses.slf4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import java.lang.invoke.MethodHandles;
+import java.util.stream.IntStream;
+
+public class LoggerRoot {
+
+    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup()
+		.lookupClass().getSimpleName()); // alternative to LoggerRoot.class.getName()
+    
+    public static void main(String[] args) {
+	
+	MDC.put("app", "SLF4J");
+	
+	IntStream.rangeClosed(1, 10).forEach(counter -> {
+	    logger.info("Counter:{}", counter);
+	});
+	
+    }   
+}
